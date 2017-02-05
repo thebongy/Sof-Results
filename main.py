2377a499b40b9a2da03e5613b4b6cf28996ae345
thebongy <rishit.bansal0@gmail.com>
2017-02-05 20:23:32 +0530

    Solvved the bug that caused not found roll nos to skip sections. ALso solved the wrong time execution bug

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import requests
 import threading
-from time import sleep,clock
+from time import sleep,time
 std_code = 'KA0138'
 std_class = 11
 olymp = 'NCO'
@@ -8,7 +8,7 @@ sections = ['A','B','C','D']
 limit = 100
 conc = 30
 URL = 'http://results.sofworld.org/results'
-rolls = [str(i).zfill(3) for i in range(limit)]
+rolls = [str(i).zfill(3) for i in range(1,limit+1)]
 olymps = {'NCO':'n'}
 
 results = []
@@ -18,43 +18,46 @@ else:
 	olymp = 'n'
 
 
-	
-data = {'form_id': 'ac_result_cards_enter_rollid_form', 'rollid1': std_code, 'rollid3': sections[0], 'rollid2': std_class, 'olympiad_selected': olymp}
 
+data = {'form_id': 'ac_result_cards_enter_rollid_form', 'rollid1': std_code, 'rollid3': sections[0], 'rollid2': std_class, 'olympiad_selected': olymp}
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 
 def get_roll_result(initial=None):
 	while len(rolls) != 0:
 		id4 = rolls.pop()
 		payload = dict(data)
 		payload['rollid4'] = id4
-		success = False
+		success=False
 		for section in sections:
 			while not success:
 				payload['rollid3'] = section
 				try:
-					r = requests.post(URL, data=payload, timeout=5)
+					r = requests.post(URL, data=payload, headers=headers,timeout=3)
 					success=True
 				except:
 					success = False
 			if r.text.find('wrong') == -1:
 				results.append((id4,section))
 				break
+			else:
+				success=False
 
 def main():
-	t=clock()
 	threads = []
 	for i in range(conc):
 		x = threading.Thread(target=get_roll_result)
 		x.start()
 		threads.append(x)
-	
-	
-	
+
+
+
 	for thread in threads:
 		thread.join()
-	
-	print clock()-t
+
+
+t=time()
 main()
+print time()-t
 
 for result in results:
 	print result
