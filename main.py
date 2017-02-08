bd61abb3bcf262ae45179a7aa6e3c24e699b526f
hubofKoushik999 <kshk.d999@gmail.com>
2017-02-08 22:44:05 +0530

    Here's a status update ;)

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import requests
 import threading
 from time import sleep,time
-std_code = 'KA0466'
-std_class = 11
-olymp = 'IMO'
-sections = ['A','B','C','D','E','F','G','H','I','J']
-limit = 400
-conc = 50
+
+
+std_code = raw_input('Enter the school code: ')
+std_class = raw_input('Enter the class: ')
+olymp = raw_input("Choose an olympiad: ")
+
+section_end = ord(raw_input("Enter the last section: ").upper())
+sections = [chr(ch) for ch in range(65,section_end+1)]
+
+limit = 50*len(sections)
+conc = limit/3
 URL = 'http://results.sofworld.org/results'
 rolls = [str(i).zfill(3) for i in range(1,limit+1)]
 olymps = {'NCO':'n','IMO':'im','NSO':'z','ISKO':'s','ICSO':'m'}
@@ -18,9 +23,9 @@ else:
 	olymp = 'n'
 
 fields = ['Student Name:','School Rank:','Roll No:','International Rank:','Qualified For 2nd Level:']
-
 data = {'form_id': 'ac_result_cards_enter_rollid_form', 'rollid1': std_code, 'rollid3': sections[0], 'rollid2': std_class, 'olympiad_selected': olymp}
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+
 
 def get_roll_result():
 	while len(rolls) != 0:
@@ -28,6 +33,7 @@ def get_roll_result():
 		payload = dict(data)
 		payload['rollid4'] = id4
 		success=False
+		
 		for section in sections:
 			while not success:
 				payload['rollid3'] = section
@@ -36,6 +42,7 @@ def get_roll_result():
 					success=True
 				except:
 					success = False
+					
 			if r.text.find('wrong') == -1:
 				std_data = []
 				for field in fields:
@@ -48,6 +55,7 @@ def get_roll_result():
 				
 			else:
 				success=False
+				
 
 def main():
 	threads = []
@@ -55,8 +63,6 @@ def main():
 		x = threading.Thread(target=get_roll_result)
 		x.start()
 		threads.append(x)
-
-
 
 	for thread in threads:
 		thread.join()
@@ -66,6 +72,7 @@ t=time()
 main()
 print time()-t
 
+
 def filterrolls(x):
         if x[1].isdigit():
                 return int(x[1])
@@ -73,7 +80,7 @@ def filterrolls(x):
                 return x[1]
 
 results.sort(key=filterrolls)
-formatting = '%40s%3s%20s%7s%4s'
+formatting = '%30s %15s %18s %23s %8s'
 print formatting % tuple(fields)
 for result in results:
 	print formatting % tuple(result)
